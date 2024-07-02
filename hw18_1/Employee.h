3232badfe4d83c38f3811228467f49fe18258fef
Yaroslav <yaroslav@keyoneinc.uk>
2024-07-02 01:42:00 +0100

    added structure Employee

---
+++ hw18_1/Employee.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+#include <iostream>
+using namespace std;
+
+struct Employee {
+    string name;
+    int birthDate;
+    string position;
+    double salary;
+    string education;
+
+};
