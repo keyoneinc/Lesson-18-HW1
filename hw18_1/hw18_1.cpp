e2338fce8962c4e3c3c8c0693dc3b617680220aa
Yaroslav <yaroslav@keyoneinc.uk>
2024-07-02 01:42:54 +0100

    added employee array in main, added 5 employees to the array

--- hw18_1/hw18_1.cpp
+++ hw18_1/hw18_1.cpp
@@ -1,7 +1,13 @@
-#include <iostream>
+#include "Employee.h"
 
 int main() {
-
-
+    int count = 5;
+    Employee* arr = new Employee[count]{
+        {"Olivia", 1943, "Software Engineer", 20000, "full"},
+        {"Liam", 1996, "Marketing Manager", 22000, "basic"},
+        {"Ava", 1939, "Financial Analyst", 23000, "basic"},
+        {"Noah", 1986, "Graphic Designer", 25000, "full"},
+        {"Isabella", 1977, "School Teacher", 19000, "full"}
+    };
 
 }
